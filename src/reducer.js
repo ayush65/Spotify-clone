2eeabd7ea0b49c58a8aa571c5644bfef10ac2669
ayush65 <72789516+ayush65@users.noreply.github.com>
2021-07-17 12:38:40 +0530

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,7 @@ export  const initialState = {
     playlists: [] ,
     playing: false,
     item: null,
-    //token: '1pYUYRkUJjWCuuq8vZaGgy97FyCRzNo7JusaTYbCJSnXwzcyqOzdRlvbw6FKyDZj5XU1bsLKFqcg7bnu2_L-oqB3JDF7ugmT91BnQdy4VqMmR4ek9id4C4GRIw_cwKIW1W7GIW5lN35CbrkgmsGHYky_1u_gfe1sBELNc6XD_cbdcOlv',
+    token: '1pYUYRkUJjWCuuq8vZaGgy97FyCRzNo7JusaTYbCJSnXwzcyqOzdRlvbw6FKyDZj5XU1bsLKFqcg7bnu2_L-oqB3JDF7ugmT91BnQdy4VqMmR4ek9id4C4GRIw_cwKIW1W7GIW5lN35CbrkgmsGHYky_1u_gfe1sBELNc6XD_cbdcOlv',
 
 
 };
